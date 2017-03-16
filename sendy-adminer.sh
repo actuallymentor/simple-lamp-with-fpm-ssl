5a6908f514795a636648390f611497e5be68a2a4
Mentor <mentor@palokaj.co>
2017-03-16 17:09:52 +0100

    Sendy and adminer modules

---
+++ sendy-adminer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y php-mcrypt php-mbstring php7.0-curl php7.0-xml php7.0-gd
+phpenmod mcrypt
+phpenmod mbstring
+phpenmod curl
+phpenmod xml
+phpenmod xmlreader
+phpenmod simplexml
+phpenmod gd
+service php7.0-fpm restart
+mkdir /var/www/localhost/adminer
+rm /var/www/localhost/index.php
+wget -O /var/www/localhost/adminer/index.php https://www.adminer.org/latest-mysql-en.php
